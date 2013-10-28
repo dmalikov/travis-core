273429b31086707b63057b5bc9702f37aee30096
Sven Fuchs <me@svenfuchs.com>
2013-10-28 14:47:53 +0100

    Revert "removed unused job and build archive api payloads"
    This reverts commit 59b7b7c26c1911de7ba36136e820c45afd832049.

--- lib/travis/api/v1.rb
+++ lib/travis/api/v1.rb
@@ -1,6 +1,7 @@
 module Travis
   module Api
     module V1
+      autoload :Archive, 'travis/api/v1/archive'
       autoload :Http,    'travis/api/v1/http'
       autoload :Helpers, 'travis/api/v1/helpers'
       autoload :Webhook, 'travis/api/v1/webhook'
